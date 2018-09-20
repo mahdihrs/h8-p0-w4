5e2c0adace40a1498f48ff7fb2daabeb3acc90c8
Mahdihrs <mahdiharis@outlook.com>
2018-09-20 13:57:33 +0700

    mencari modus dalam array

--- [Exercises 4] Cari Modus.js
+++ [Exercises 4] Cari Modus.js
@@ -1,28 +1,30 @@
-/*
-  kalo ga ada yang sama berarti lanjut ke nomor selanjutnya [i + 1];
-  berarti data array tsb dikeluarin dari array
-
-
-*/
-
 function cariModus(arr) {
-  var modus = [];
-  var arrMod = true;
-  var arrNum = 0;
-  // console.log(arrNum);
-  for (var i = 0; i < arr.length; i++) {
-    if (arrMod !== arr[i]) {
-      arrNum = arrNum + arr[i + 1];
-      // return arrNum;
-    } else if (arrNum === arr[i]) {
+  var angka = 0;
+  var pembanding = 0;
 
+  for (var i = 0; i < arr.length; i++) {
+    var counter = 0;
+    for (var j = 0; j < arr.length; j++) {
+      if (arr[i] === arr[j]) {
+        counter += 1;
+      }
+    }
+    if (counter > pembanding) {
+      pembanding = counter;
+      angka = arr[i];
     }
   }
+  if ( pembanding === 1) {
+    return -1;
+  } else if (pembanding === arr.length) {
+    return -1;
+  }
+  return angka;
 }
 
 // TEST CASES
 console.log(cariModus([10, 4, 5, 2, 4])); // 4
-// console.log(cariModus([5, 10, 10, 6, 5])); // 5
-// console.log(cariModus([10, 3, 1, 2, 5])); // -1
-// console.log(cariModus([1, 2, 3, 3, 4, 5])); // 3
-// console.log(cariModus([7, 7, 7, 7, 7])); // -1
+console.log(cariModus([5, 10, 10, 6, 5])); // 5
+console.log(cariModus([10, 3, 1, 2, 5])); // -1
+console.log(cariModus([1, 2, 3, 3, 4, 5])); // 3
+console.log(cariModus([7, 7, 7, 7, 7])); // -1
